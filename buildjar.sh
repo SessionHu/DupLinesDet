a5738387fe862966e88caff38d29bc18efb03b3c
SessionHu <102411014+SessionHu@users.noreply.github.com>
2023-07-27 15:34:56 +0800

    chore(buildjar.sh): 增加提示

--- buildjar.sh
+++ buildjar.sh
@@ -1,8 +1,11 @@
+echo -e "正在处理上一次构建的内容..."
 rm -r build/
 mkdir build/
 
+echo -e "正在编译..."
 javac -d build/ DuplicateLines.java
 
+echo -e "正在打包..."
 cp ./README.md ./LICENSE build/
 cd build/
 jar -cvfm 'DuplicateLines.jar' ../manifest -C ./ .
